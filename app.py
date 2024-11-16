fe2b45dc04a859c8ed835717a79f14de742e80f7
Tharushi Kawodya Arachchige <139765472+kawodyaarachchige@users.noreply.github.com>
2024-11-16 22:19:00 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,48 @@
+#a =200
+#print("Value ",a,"is",type(a))
+
+#b =12.89
+#print("value",b,"is",type(b))
+#print("Type of variable having value",b,"is",type(b))
+
+#c = 100 + 5j
+#print(c) 
+
+#num_1=2.5e4+5j
+#print(num_1,"and type is",type(num_1))
+
+#num_2=7
+#num_3=12.2
+#b=int(num_3)
+#print(b,type(b))
+
+#python collection(we can add any of types data with collection)
+#list
+#my_list =["Red","Pink","Blue","Green",2]
+#length of the list
+#print(len(my_list))
+#print(my_list,type(my_list))
+#get blue elmenet we can use its index,so indexes are starting 0
+#print(my_list[2])
+
+#we can add new value and remove value in list(changable)
+#my_list[2]="purple"
+#print(my_list)
+#can acces last value using -1,-2........
+#print(my_list[-1])
+#list are orderd
+#add value
+#my_list.append(False)
+#print(my_list)
+#remove value
+#my_list.remove("Pink")
+#its allowed duplicate values
+
+#ramge of value that we want
+x = 1000**10
+y = 1000**10
+print(x is y)
+
+
+
+
